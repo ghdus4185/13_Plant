5d32a2d5019ee930dd2e9d809c7baca87799b357
ghdus4185 <ckdghdus@naver.com>
2019-10-22 16:55:28 +0900

    좋아요 완성

--- todos/models.py
+++ todos/models.py
@@ -9,10 +9,17 @@ class Write(models.Model):
     url = models.CharField(max_length=200)
     created_at = models.DateField(auto_now_add=True)
     cnt = models.IntegerField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="like_posts")
+
 
 class Comment(models.Model):
     comment = models.CharField(max_length=100)
     created_at = models.DateField(auto_now_add=True)
     write = models.ForeignKey(Write, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    comment_like_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="comment_like_posts")
--- todos/views.py
+++ todos/views.py
@@ -1,84 +1,92 @@
-from django.shortcuts import render,redirect,get_object_or_404
-from .models import Write,Comment
-from .forms import WriteForm,CommentForm
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Write, Comment
+from .forms import WriteForm, CommentForm
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 import random
 # Create your views here.
+
 
 def index(request):
     writes = Write.objects.all()
     context = {
         'writes': writes
     }
-    return render(request,'index.html', context)
+    return render(request, 'index.html', context)
+
 
 @login_required
 def create(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         form = WriteForm(request.POST)
         if form.is_valid():
-            writes=form.save(commit=False)
+            writes = form.save(commit=False)
             writes.user = request.user
             writes.cnt = 0
             writes.save()
             return redirect('todos:index')
     else:
         form = WriteForm()
-    context={
+    context = {
         'form': form,
     }
-    return render(request,'post.html',context)
+    return render(request, 'post.html', context)
 
-def detail(request,id):
+
+def detail(request, id):
     write = get_object_or_404(Write, id=id)
     comment_form = CommentForm()
     write.cnt += 1
     write.save()
     context = {
         'write': write,
-        'comment_form':comment_form,
+        'comment_form': comment_form,
     }
     return render(request, 'detail.html', context)
 
-def delete(request,id):
-    if request.method=='POST':
-        get_object_or_404(Write,id=id).delete()
+
+def delete(request, id):
+    if request.method == 'POST':
+        get_object_or_404(Write, id=id).delete()
         return redirect('todos:index')
     else:
         return redirect('todos:index')
 
-def update(request,id):
-    write=get_object_or_404(Write,id=id)
-    if request.method=='POST':
+
+def update(request, id):
+    write = get_object_or_404(Write, id=id)
+    if request.method == 'POST':
         form = WriteForm(request.POST, instance=write)
         if form.is_valid():
             form.save()
-            
-            return redirect('todos:detail',id)
+
+            return redirect('todos:detail', id)
 
     else:
         form = WriteForm(instance=write)
     context = {
-        'form':form,
+        'form': form,
     }
-    return render(request,'post.html', context)
+    return render(request, 'post.html', context)
+
 
 @require_POST
-def comment_create(request,id):
-    write = get_object_or_404(Write,id=id)
+def comment_create(request, id):
+    write = get_object_or_404(Write, id=id)
     comments_form = CommentForm(request.POST)
     if comments_form.is_valid():
         comment_ = comments_form.save(commit=False)
         comment_.write = write
         comment_.user = request.user
         comment_.save()
-    return redirect('todos:detail',id)
+    return redirect('todos:detail', id)
+
 
 @require_POST
-def comment_delete(request,id1,id2):
-    get_object_or_404(Comment,id=id2).delete()
-    return redirect('todos:detail',id1)
+def comment_delete(request, id1, id2):
+    get_object_or_404(Comment, id=id2).delete()
+    return redirect('todos:detail', id1)
+
 
 def search(request):
     if request.method == 'POST':
@@ -96,3 +104,23 @@ def search(request):
             'writes': writes
         }
         return render(request, 'search.html', context)
+
+
+def like(request, id):
+    post = get_object_or_404(Write, id=id)
+    user = request.user
+    if user in post.like_users.all():
+        post.like_users.remove(user)
+    else:
+        post.like_users.add(user)
+    return redirect('todos:detail', id)
+
+
+def comment_like(request, write_id, comment_id):
+    post = get_object_or_404(Comment, id=comment_id)
+    user = request.user
+    if user in post.comment_like_users.all():
+        post.comment_like_users.remove(user)
+    else:
+        post.comment_like_users.add(user)
+    return redirect('todos:detail', write_id)
